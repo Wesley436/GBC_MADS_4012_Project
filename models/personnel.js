91422bcf757767899b4a9cf1d7d46438b3451364
WesleyHui <72075816+Wesley436@users.noreply.github.com>
2025-06-09 00:37:14 -0400

    templating

--- models/personnel.js
+++ models/personnel.js
@@ -10,11 +10,26 @@ const schema = new mongoose.Schema(
             type: String,
             required: true
         },
-        genre: {
+        skills: {
             type: [String]
         }
     }
 );
+
+/** 
+ *  Validates whether necessary fields are empty and add errors to an array if not
+ *  @param {Object} personnel - The personnel object to be validated
+ *  @param {List<String>} errors - A list storing validation errors (if any)
+ */
+function validatePersonnel(personnel, errors) {
+    if(personnel.title === "") {
+        errors.push("You must enter a name");
+    }
+
+    if(personnel.rank === "") {
+        errors.push("You must select a rank");
+    }
+}
 
 const Personnel = mongoose.model('Personnel', schema);
 
--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const personnel = require("./models/personnel.js");
+const Personnel = personnel.Personnel;
+const ship = require("./models/ship.js");
+const Ship = ship.Ship;
+const mission = require("./models/mission.js");
+const Mission = mission.Mission;
 
 require("dotenv").config();
 const db = require("./config/db.js");
@@ -11,6 +17,97 @@ db.dbInit();
 
 app.get("/", async function(req,res) {
     res.render("home.ejs");
+});
+
+app.get("/personnel", async function(req, res) {
+    const personnels = await Personnel.find();
+    res.render("personnel.ejs", {personnels: "personnels"});
+});
+
+app.post("/personnel", async function(req, res) {
+    const errors = [];
+    newPersonnel = req.body;
+    
+    personnel.validatePersonnel(newPersonnel, errors);
+
+    if(errors.length > 0) {
+        return res.render("create_personnel.ejs", {errors, id: null, personnel: newPersonnel, action_type: "create"});
+    }
+
+    const newPersonnelModel = new Personnel(newPersonnel);
+    await newPersonnelModel.save();
+
+    res.redirect("/personnel");
+});
+
+app.put("/personnel/:id", async function(req, res) {
+    const id = req.params.id;
+
+    if (!id || id.length !== 24) {
+        res.redirect("/");
+        return;
+    }
+    
+    const errors = [];
+    currentPersonnel = req.body;
+    
+    personnel.validatePersonnel(currentPersonnel, errors);
+
+    if(errors.length > 0) {
+        return res.render("create_personnel.ejs", {errors, id: null, personnel: currentPersonnel, action_type: "edit"});
+    }
+
+    await Personnel.findOneAndUpdate(
+        {_id: id},
+        {
+            $set: {
+                name: currentPersonnel.name,
+                rank: currentPersonnel.rank,
+                skills: currentPersonnel.skills
+            }
+        },
+        {new: true, runValidators: true}
+    );
+
+    res.redirect("/personnel");
+});
+
+app.delete("/personnel/:id", async function(req, res) {
+    
+});
+
+app.get("/ship", async function(req, res) {
+    const ships = await Ship.find();
+    res.render("ship.ejs", {ships: "ships"});
+});
+
+app.post("/ship", async function(req, res) {
+    
+});
+
+app.put("/ship/:id", async function(req, res) {
+    
+});
+
+app.delete("/ship/:id", async function(req, res) {
+    
+});
+
+app.get("/mission", async function(req, res) {
+    const missions = await Mission.find();
+    res.render("mission.ejs", {missions: "missions"});
+});
+
+app.post("/mission", async function(req, res) {
+    
+});
+
+app.put("/mission/:id", async function(req, res) {
+    
+});
+
+app.delete("/mission/:id", async function(req, res) {
+    
 });
 
 const PORT = 3001;
